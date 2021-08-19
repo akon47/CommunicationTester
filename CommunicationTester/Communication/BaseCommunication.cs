e63181094a913014b69fd734a5cf8cf4f021b955
akon47 <akon47@naver.com>
2021-08-19 09:09:23 +0900

    통신을 위한 기본 베이스 클래스 작성

---
+++ CommunicationTester/Communication/BaseCommunication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationTester.Communication
+{
+	public abstract class BaseCommunication : NotifyPropertyBase, IDisposable
+	{
+		private Status status = Status.None;
+		public Status Status
+		{
+			get => status;
+			protected set => SetProperty(ref status, value);
+		}
+
+		public event DataReceivedHandler DataReceived;
+
+		protected void OnDataReceived(byte[] data)
+		{
+			if(data != null)
+			{
+				DataReceived?.Invoke(this, new DataReceivedEventArgs(data));
+			}
+		}
+
+		public virtual void Dispose()
+		{
+			
+		}
+	}
+}
---
+++ CommunicationTester/Communication/DataReceived.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationTester.Communication
+{
+	public class DataReceivedEventArgs : EventArgs
+	{
+		public byte[] Data { get; private set; }
+
+		public DataReceivedEventArgs(byte[] data)
+		{
+			this.Data = data;
+		}
+	}
+
+	public delegate void DataReceivedHandler(object sender, DataReceivedEventArgs e);
+}
